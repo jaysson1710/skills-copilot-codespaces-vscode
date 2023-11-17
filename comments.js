60d83adad3ee1715942c0f3d5e307548d82a6a3a
jaysson.avendano <131981078+JayssonAvendanoSWO@users.noreply.github.com>
2023-11-17 01:50:34 +0000

    add file

--- comments.js
+++ comments.js
@@ -1,21 +1,46 @@
 //create web server
+//http://localhost:3000/comments
 const express = require("express");
 const router = express.Router();
-const {
-  getComments,
-  getComment,
-  createComment,
-  updateComment,
-  deleteComment,
-} = require("../controllers/comments");
-const { protect, authorize } = require("../middleware/auth");
+const { Comment } = require("../models/Comment");
+const { auth } = require("../middleware/auth");
 
-router.route("/").get(protect, getComments).post(protect, createComment);
+//=================================
+//             Subscribe
+//=================================
 
-router
-  .route("/:id")
-  .get(protect, getComment)
-  .put(protect, updateComment)
-  .delete(protect, deleteComment);
+router.post("/saveComment", auth, (req, res) => {
+  //save the comment information from the client into the database
+  const comment = new Comment(req.body);
+
+  comment.save((err, comment) => {
+    //save the comment information from the client into the database
+    if (err) return res.json({ success: false, err });
+
+    //find the comment information from the client into the database
+    Comment.find({ _id: comment._id })
+      .populate("writer") //populate the writer information
+      .exec((err, result) => {
+        //find the comment information from the client into the database
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({ success: true, result });
+      });
+  });
+});
+
+//=================================
+//             Get Comments
+//=================================
+
+router.post("/getComments", (req, res) => {
+  //find the comment information from the client into the database
+  Comment.find({ postId: req.body.movieId })
+    .populate("writer") //populate the writer information
+    .exec((err, comments) => {
+      //find the comment information from the client into the database
+      if (err) return res.status(400).send(err);
+      res.status(200).json({ success: true, comments });
+    });
+});
 
 module.exports = router;
